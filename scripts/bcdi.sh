2b116d7812e3eb80f654c5d1effab632b53c4a38
Florestan Bredow <daiko@daiko.fr>
2015-06-30 13:58:53 +0200

    script bcdi pour installer une VM automatiquement

---
+++ scripts/bcdi.sh
@@ -0,0 +1,37 @@
+#/bin/bash
+
+VM='bcdi'
+FOLDER="$HOME/.virtualbox/$VM"
+
+# Vérifie si l'utilisateur est dans le groupe vboxusers
+if ! groups bredow | grep -q "vboxusers"; then
+    # Sinon le rajoute.
+    sudo adduser $USER vboxusers
+fi
+
+mkdir -p ~/"$FOLDER"
+
+# Créé la machine virtuelle
+VBoxManage createvm --name "$VM" \
+                    --ostype "WindowsXP" \
+                    --basefolder "$FOLDER/.." \
+                    --register 
+
+VBoxManage modifyvm "$VM" --memory 512 \
+                          --acpi on \
+                          --nic1 bridged \
+                          --bridgeadapter1 eth0
+
+VBoxManage storagectl "$VM" --name "IDE Controller" \
+                            --add ide
+
+# Télécharge le disque a partir du serveur
+cd "$FOLDER"
+wget http://serveur/bcdi.vdi
+cd -
+
+VBoxManage storageattach "$VM" --storagectl "IDE Controller" \
+                               --port 0 \
+                               --device 0 \
+                               --type hdd \
+                               --medium "$FOLDER/$VM.vdi"
