d874231cbd4694eed5cd2d9cf77cdc97664dcdc3
Florestan Bredow <daiko@daiko.fr>
2015-06-30 11:46:45 +0200

    Script pour virtualbox

--- scripts/gconf.sh
+++ scripts/gconf.sh
@@ -2,3 +2,4 @@
 
 # Supprime la fenetre de mise a jour de la version
 gconftool --set --config-source xml:readwrite:/etc/gconf/gconf.xml.mandatory --type bool /apps/update-notifier/auto_launch false 
+true
---
+++ scripts/virtualbox.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+apt remove virtualbox virtualbox-dkms
+
+echo 'deb http://download.virtualbox.org/virtualbox/debian trusty contrib
+' > /etc//etc/apt/sources.list.d/virtualbox.list
+
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- \
+    | sudo apt-key add - 
+
+apt update
+
+apt -y install virtualbox-4.3 build-essential dkms linux-source linux-headers-`uname -r`
+
+# Ajoute la gestion de l'USB2
+version=$(VBoxManage --version|cut -dr -f1|cut -d'_' -f1) && 
+cd /tmp
+wget -c "http://download.virtualbox.org/virtualbox/$version/Oracle_VM_VirtualBox_Extension_Pack-$version.vbox-extpack"
+VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-$version.vbox-extpack
+rm *.vbox-extpack
+cd -
+
