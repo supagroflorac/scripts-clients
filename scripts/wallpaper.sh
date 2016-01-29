1bc1b33a1e7c41a3d85f2bf7ad4cf043d8b72d62
Florestan Bredow <florestan.bredow@supagro.fr>
2016-01-29 11:29:30 +0100

    Changement fond d'écran par défaut

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,7 +1,5 @@
 #/bin/bash
 
 # Modification du fond d'écran
-mv -f /usr/share/backgrounds/warty-final-ubuntu.png \
-      /usr/share/backgrounds/warty-final-ubuntu2.png &&
 cp data/warty-final-ubuntu.png /usr/share/backgrounds/ &&
 chmod o+r /usr/share/backgrounds/warty-final-ubuntu.png 
