d3b2193bcd41ce88e349b397119ceb02850f8733
Florestan Bredow <daiko@daiko.fr>
2016-05-27 15:38:36 +0200

    Enleve les applications qui n'existent plus

--- scripts/software.sh
+++ scripts/software.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Utilitaires
-apt -y install vim aptitude ssh &&
-
 # Accepte la licence pour les polices Microsoft.
 echo 'ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true' | debconf-set-selections &&
 
 # Installation des logiciels contenus dans les dépots
-apt -y install freemind gimp geany vlc r-base inkscape scribus audacity vim \
-    flashplugin-installer kdenlive ttf-mscorefonts-installer ssh freecad \
-    icedtea6-plugin qgis gdal-bin avidemux pdfsam aptitude pdfshuffler
+apt -y install vim ssh freeplane gimp geany vlc r-base inkscape scribus \
+    audacity adobe-flashplugin ttf-mscorefonts-installer freecad libreoffice \
+    icedtea-plugin pdfsam pdfshuffler openshot kdenlive
