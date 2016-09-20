717c07d5264f3fae8644a48ec13e98ce48ca5c62
Florestan Bredow <daiko@daiko.fr>
2016-09-20 17:51:05 +0200

    Ajoute un délais de 10s avant de redémarrer le service nslcd

--- scripts/ldap.sh
+++ scripts/ldap.sh
@@ -40,29 +40,10 @@ base passwd ou=Users,dc=florac,dc=meta
 map passwd loginshell \"/bin/bash\"
 " > /etc/nslcd.conf &&
 
-echo "#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will \"exit 0\" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-
-service nslcd restart
-
-exit 0
-" > "/etc/rc.local" &&
-
-
 
 # Ajoute le redémarrage de nslcd a la fin du boot de la machine
 if ! grep -q 'service nslcd restart' /etc/rc.local ; then
-    sed -i "s#exit 0#service nslcd restart\n\nexit 0#" /etc/rc.local
+    sed -i "s#exit 0#sleep 10s && service nslcd restart\n\nexit 0#" /etc/rc.local
 fi
 
 
